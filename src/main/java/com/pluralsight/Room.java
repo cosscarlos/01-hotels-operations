d6eae943152bc882df8585c6fdfd3cbdfcfcd54b
carloscoss <crobertohenriquez@gmail.com>
2026-05-04 14:58:02 -0500

    Creating class, properties, constructor and getters and setters,

---
+++ src/main/java/com/pluralsight/Room.java
@@ -0,0 +1,59 @@
+package com.pluralsight;
+
+public class Room {
+    private String numberOfBeds;
+    private Double price;
+    private Boolean occupied;
+    private Boolean dirty;
+    private Boolean available;
+
+
+    public Room(String numberOfBeds, Double price, Boolean occupied, Boolean dirty, Boolean available){
+
+    }
+
+    public String getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public void setNumberOfBeds(String numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Boolean isOccupied() {
+        return occupied;
+    }
+
+    public void setOccupied(Boolean occupied) {
+        this.occupied = occupied;
+    }
+
+    public Boolean isDirty() {
+        return dirty;
+    }
+
+    public void setDirty(Boolean dirty) {
+        this.dirty = dirty;
+    }
+
+    public Boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+}
+
+
+
+
+
