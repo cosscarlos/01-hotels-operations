1a524ca04deaeb02ad9d82ad517248a2d3d2f3a2
carloscoss <crobertohenriquez@gmail.com>
2026-05-05 14:02:48 -0500

    more improvements

--- src/main/java/com/pluralsight/Hotel.java
+++ src/main/java/com/pluralsight/Hotel.java
@@ -7,13 +7,28 @@ public class Hotel {
     private int bookedSuites;
     private int bookedBasicRooms;
 
-    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
-        this.name = name;
-        this.numberOfSuites = numberOfSuites;
-        this.numberOfRooms = numberOfRooms;
-        this.bookedSuites = bookedSuites;
-        this.bookedBasicRooms = bookedBasicRooms;
+    public Hotel(String name, int numberOfSuites, int numberOfRooms) {
+     this.name = name;
+     this.numberOfSuites = numberOfSuites;
+     this.numberOfRooms = numberOfRooms;
+     this.bookedSuites = 0;
+     this.bookedBasicRooms = 0;
     }
+    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms){
+    this.name = name;
+    this.numberOfSuites = numberOfSuites;
+    this.numberOfRooms = numberOfRooms;
+    this.bookedSuites = bookedSuites;
+    this.bookedBasicRooms = bookedBasicRooms;
+    }
+
+    public int getAvailableSuites(){
+        return numberOfSuites - bookedSuites;
+    }
+    public int getAvailableRooms(){
+        return numberOfRooms - bookedBasicRooms;
+    }
+
 
 
 }
